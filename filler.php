78dafb8ccaebf567a3ff11eee3c627105ef7ed34
Elvis Oliveira <elvis.olv@gmail.com>
2022-05-16 18:15:10 -0300

    Add support for filling S-88_E

--- filler.php
+++ filler.php
@@ -4,6 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use mikehaertl\pdftk\Pdf;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Shared\Font;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
@@ -11,6 +12,12 @@ use PhpOffice\PhpSpreadsheet\Style\Color;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
+// Path to true-type fonts containig Arial
+// Filename must be exactly: arial.ttf
+Font::setTrueTypeFontPath('/usr/share/fonts/TTF/');
+Font::setAutoSizeMethod(Font::AUTOSIZE_METHOD_EXACT);
+
+// @TODO: Make month to be an ARG input as well
 $month = readline(lang('MONTH_INPUT'));
 if(!is_numeric($month) || $month < 1 || $month > 12) die(lang('MONTH_INVALID'));
 
@@ -40,12 +47,12 @@ $columns = [
     'Remarks' => 'Observation'
 ];
 $segments = [
-    'P' => [0, 0, 0, 0, 0, 0], // Publishers
-    'R' => [0, 0, 0, 0, 0, 0], // Regular Pioneers
-    'A' => [0, 0, 0, 0, 0, 0]  // Auxiliary Pioneers
+    'P' => [0, 0, 0, 0, 0, 0, 'Publishers'],
+    'R' => [0, 0, 0, 0, 0, 0, 'Regular Pioneers'],
+    'A' => [0, 0, 0, 0, 0, 0, 'Auxiliary Pioneers']
 ];
 $monthName = $months[$month];
-$serviceYear = 2022;
+$serviceYear = 2022; // @TODO: Make year to be an arg and user input
 $directory = sprintf("%s/pdf/%s", getcwd(), lang('FOLDER_PUBLISHER'));
 $prefix = 1;
 $suffix = $prefix > 1 ? "_{$prefix}" : '';
@@ -82,10 +89,12 @@ uasort($reports, function ($one, $two) {
     return $one[0] <=> $two[0];
 });
 
-if ($handle = opendir($directory))
-{
+if ($handle = opendir($directory)) {
     $spreadsheet = new Spreadsheet();
+    $spreadsheet->getDefaultStyle()->getFont()->setName('Arial');
+    $spreadsheet->getDefaultStyle()->getFont()->setSize(11);
     $spreadsheet->setActiveSheetIndex(0);
+
     $publisherSheet = $spreadsheet->getActiveSheet()->freezePane('A2');
     $publisherSheet->setTitle(lang('FOLDER_PUBLISHER'));
     $publisherSheet->fromArray(array_merge(['', ''], array_values($columns)), null, "A1");
@@ -96,8 +105,7 @@ if ($handle = opendir($directory))
             $segments[$report[0]][$i] += $report[$i + 1];
         }
         $segments[$report[0]][5]++;
-        if (preg_match('/\.pdf$/', $fileName) && isset($report))
-        {
+        if (preg_match('/\.pdf$/', $fileName) && isset($report)) {
             $data["Service Year{$suffix}"] = $serviceYear;
             $indexColumns = array_keys($columns);
             for ($i = 0; $i <= 4; $i++) {
@@ -126,22 +134,41 @@ if ($handle = opendir($directory))
     }
     closedir($handle);
 
-    setAutoSize($publisherSheet);
+    setSizeAndColors($publisherSheet);
 
     array_pop($columns);
 
     $spreadsheet->createSheet();
     $spreadsheet->setActiveSheetIndex(1);
+
     $publisherTotalSheet = $spreadsheet->getActiveSheet()->freezePane('A2');
     $publisherTotalSheet->fromArray(array_merge([''], array_values($columns), ['Number of Reports']), null, "A1");
     $publisherTotalSheet->setTitle(lang('TAB_TOTALS'));
+
     $index = 2;
     foreach($segments as $privilege => $data) {
+        if($runPDF) {
+            $fill = [];
+            foreach(array_keys($columns) as $i => $column) {
+                $fill["{$prefix}-{$column}_{$month}"] = intval($data[$i]);
+            }
+            $fill["{$indexColumns[5]}{$monthName}{$suffix}"] = intval($data[5]);
+
+            $file = sprintf("%s/../%s/%s.pdf", $directory, lang('FOLDER_TOTALS'), $data[6]);
+            $pdf = new Pdf($file);
+            $pdf->fillForm($fill);
+            if (!$pdf->saveAs($file)) {
+                die($pdf->getError());
+            }
+
+            print $data[6] . PHP_EOL;
+        }
+        unset($data[6]);
         $publisherTotalSheet->fromArray(array_merge([$privilege], $data), null, "A{$index}");
         $index++;
     }
 
-    setAutoSize($publisherTotalSheet);
+    setSizeAndColors($publisherTotalSheet);
 
     $spreadsheet->createSheet();
     $spreadsheet->setActiveSheetIndex(2);
@@ -149,83 +176,132 @@ if ($handle = opendir($directory))
     $attendenceSheet->fromArray(['', 'Total', 'Foreigners Only'], null, "A1");
     $attendenceSheet->setTitle(lang('TAB_ATTENDENCE'));
 
+    // 0: Number of meetings
+    // 1: Attendance (Total)
+    // 2: Attendance (Foreigners Only)
+    $weekend = [0, 0, 0];
+    $midweek = [0, 0, 0];
+
     $index = 2;
     foreach($meetings as $meeting) {
+        $date = DateTime::createFromFormat('Y-m-d', $meeting[0]);
+
+        $isWeekend = isWeekend($date);
+        if(!is_null($isWeekend)) {
+            if($isWeekend) {
+                $weekend[0]++;
+                $weekend[1]+=$meeting[1];
+                $weekend[2]+=$meeting[2];
+            }
+            else {
+                $midweek[0]++;
+                $midweek[1]+=$meeting[1];
+                $midweek[2]+=$meeting[2];
+            }
+        }
+
         $attendenceSheet->fromArray($meeting, null, "A{$index}");
         $index++;
     }
 
-    foreach ($attendenceSheet->getRowIterator() as $row) {
-        $rowId = $row->getRowIndex();
-        foreach($row->getCellIterator() as $column) {
-            $bg = Color::COLOR_WHITE;
-            $columnId = $column->getColumn();
-            $cell = "{$columnId}{$rowId}";
-            $cellValue = $attendenceSheet->getCell($cell)->getValue();
+    if($runPDF) {
+        $_prefix = $prefix + 2;
+        $attendance = [
+            'Report of Meeting Attendance - Foreigners' => [
+                "{$prefix}-Meeting_{$month}" => $midweek[0],
+                "{$prefix}-Attendance_{$month}" => $midweek[2],
+                "{$prefix}-Average_{$month}" => round($midweek[2] / $midweek[0], 2),
+                "{$_prefix}-Meeting_{$month}" => $weekend[0],
+                "{$_prefix}-Attendance_{$month}" => $weekend[2],
+                "{$_prefix}-Average_{$month}" => round($weekend[2] / $weekend[0], 2)
+            ],
+            'Report of Meeting Attendance' => [
+                "{$prefix}-Meeting_{$month}" => $midweek[0],
+                "{$prefix}-Attendance_{$month}" => $midweek[1],
+                "{$prefix}-Average_{$month}" => round($midweek[1] / $midweek[0], 2),
+                "{$_prefix}-Meeting_{$month}" => $weekend[0],
+                "{$_prefix}-Attendance_{$month}" => $weekend[1],
+                "{$_prefix}-Average_{$month}" => round($weekend[1] / $weekend[0], 2)
+            ]
+        ];
 
-            if($columnId == "A" && $cellValue <> NULL) {
-                $date = DateTime::createFromFormat('Y-m-d', $cellValue);
-                $format = true ? 'NNNNMMMM DD, YYYY' : NumberFormat::FORMAT_DATE_YYYYMMDD;
-
-                $attendenceSheet->setCellValue($cell, Date::PHPToExcel($date));
-                $attendenceSheet->getStyle($cell)->getNumberFormat()->setFormatCode($format);
-
-                switch ($date->format('l')) {
-                    case "Sunday":
-                    case "Saturday":
-                        $bg = Color::COLOR_YELLOW;
-                        break;
-                    case "Monday":
-                    case "Tuesday":
-                    case "Wednesday":
-                    case "Thursday":
-                    case "Friday":
-                        $bg = Color::COLOR_RED;
-                        break;
-                }
+        foreach($attendance as $file => $reports) {
+            $path = sprintf("%s/../%s/%s.pdf", $directory, 'Meeting Attendence', $file);
+            $pdf = new Pdf($path);
+            $pdf->fillForm($reports);
+            if (!$pdf->saveAs($path)) {
+                die($pdf->getError());
             }
-
-            $attendenceSheet->getStyle($cell)->applyFromArray(getStyle($cellValue, $bg));
-            $attendenceSheet->getColumnDimension($columnId)->setAutoSize(true);
         }
     }
+
+    setSizeAndColors($attendenceSheet);
 
     $writer = IOFactory::createWriter($spreadsheet, 'Xls');
     $writer->save(getcwd() . "/excel/{$serviceYear}-{$month}.xlsx");
 }
 
-function setAutoSize($sheet) {
+function setSizeAndColors($sheet) {
     foreach ($sheet->getRowIterator() as $row) {
         $rowId = $row->getRowIndex();
         foreach($row->getCellIterator() as $column) {
             $columnId = $column->getColumn();
-            $value = $sheet->getCell("{$columnId}{$rowId}")->getValue();
+            $cell = "{$columnId}{$rowId}";
+            $value = $sheet->getCell("{$cell}")->getValue();
             if($columnId == "A") {
-                switch ($value) {
-                    case "P":
-                        $bg = Color::COLOR_YELLOW;
-                        break;
-                    case "R":
-                        $bg = Color::COLOR_RED;
-                        break;
-                    case "A":
-                        $bg = Color::COLOR_GREEN;
-                        break;
-                    default:
-                        $bg = Color::COLOR_WHITE;
+                if($value == null) {
+                    continue;
+                }
+                if(preg_match("/\b(A|P|R)\b/", $value)) {
+                    switch ($value) {
+                        case "P":
+                            $bg = Color::COLOR_YELLOW;
+                            break;
+                        case "R":
+                            $bg = Color::COLOR_RED;
+                            break;
+                        case "A":
+                            $bg = Color::COLOR_GREEN;
+                            break;
+                    }
+                }
+                else if(preg_match("/\d{4}\-\d{2}-\d{2}/", $value)) {
+                    $date = DateTime::createFromFormat('Y-m-d', $value);
+                    $format = true ? 'NNNNMMMM DD, YYYY' : NumberFormat::FORMAT_DATE_YYYYMMDD;
+
+                    $sheet->setCellValue($cell, Date::PHPToExcel($date));
+                    $sheet->getStyle($cell)->getNumberFormat()->setFormatCode($format);
+
+                    $isWeekend = isWeekend($date);
+                    $bg = is_null($isWeekend) ? Color::COLOR_WHITE : $isWeekend ? Color::COLOR_YELLOW : Color::COLOR_RED;
                 }
             }
-            $sheet->getStyle("{$columnId}{$rowId}")->applyFromArray(getStyle($value, $bg));
+            $sheet->getStyle("{$cell}")->applyFromArray(getStyle($rowId == 1 ? 1 : $value, $bg ?? Color::COLOR_WHITE));
             $sheet->getColumnDimension($columnId)->setAutoSize(true);
         }
     }
 }
 
+function isWeekend($date) {
+    switch ($date->format('l')) {
+        case "Sunday":
+        case "Saturday":
+            return true;
+            break;
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+            return false;
+            break;
+    }
+    return null;
+}
+
 function getStyle($value, $bg) {
     return [
         'font' => [
-            'size'  => 11,
-            'name'  =>  'Arial',
             'color' => [
                 'argb' => Color::COLOR_BLACK
             ]
@@ -257,6 +333,7 @@ function lang($phrase) {
         'MONTH_INPUT'          => 'Input the service year month number [1-12]: ',
         'MONTH_INVALID'        => 'Invalid month',
         'FOLDER_PUBLISHER'     => 'Publisher Recordings',
+        'FOLDER_TOTALS'        => 'Congregation Totals',
         'PDF_PROCESS'          => 'Process PDF [0 = No, 1 = Yes, default = 0]: ',
         'PDF_INVALID'          => 'Invalid PDF param',
         'TAB_TOTALS'           => 'Publisher Recordings Totals',
